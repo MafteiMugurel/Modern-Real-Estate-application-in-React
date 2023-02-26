32c039067912be4968b87261498634b59dd1b9a9
MafteiMugurel <mugurel.maftei95@gmail.com>
2023-02-26 21:06:45 +0200

    updated package json

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import styles from "@/styles/Home.module.css";
-import Link from "next/link";
-import Image from "next/image";
+// import Link from "next/link";
+// import Image from "next/image";
+import { Flex } from "@chakra-ui/react";
+
+const Banner = ({ purpose }) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    {purpose}
+  </Flex>
+);
 
 export default function Home() {
-  return <div>Hello Mugur</div>;
+  return (
+    <div>
+      <h1>olla </h1>
+      <Banner purpose={"For Sale"} />
+      <Banner purpose={"For Rent"} />
+    </div>
+  );
 }
